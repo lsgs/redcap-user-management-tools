ed7fb950d4a1a0dceb68e0dab29f3eb3bfe0e911
Luke <lukestevens@hotmail.com>
2023-06-16 14:47:33 +1000

    Fixes and enable with php7.4

--- UserManagementTools.php
+++ UserManagementTools.php
@@ -18,29 +18,37 @@ class UserManagementTools extends AbstractExternalModule
 
     public function redcap_every_page_top($project_id) {
         if (empty($project_id)) return;
-        $this->user = $this?->getUser();
-        $this->username = $this->user?->getUsername();
+        if (!defined('USERID')) return;
+        $this->user = $this->getUser();
+        $this->username = $this->user->getUsername();
         if ($this->username==="[survey_respondent]") return;
-        if ($this->pagePermitted()) return;
+        if ($this->hasPagePermission()) return;
         $this->restrictPageAccess();
     }
 
     /**
-     * pagePermitted()
+     * hasPermission()
      * Only Institutional user accounts are permitted User Rights and DAGs access to projects 
      * @return bool 
      */
-    protected function pagePermitted($username=null, $page=null) {
+    protected function hasPagePermission($username=null, $userRight=null) {
         $username = $username ?? $this->username;
         $permitted = false;
         $rights = $this->getRights($username);
-        if ($rights['user_rights']=='0' && $rights['data_access_groups']=='0') $permitted = true;
 
-        $overrideList = $this->getSystemSetting('permit-as-project-admin');
-        if (preg_match("/\b$username\b/", $overrideList)) $permitted = true; // username is in permitted list
+        if (is_null($userRight)) {
+            // any/all project pages - block/warn if non-Institutional user account with admin rights
+            if ($rights['user_rights']=='0' && $rights['data_access_groups']=='0') $permitted = true;
 
-        $q = $this->query("select 1 from redcap_user_allowlist where username=? limit 1", [$username]);
-        if (db_num_rows($q)>0) $permitted = true; // user is in allow list
+            $overrideList = $this->getSystemSetting('permit-as-project-admin');
+            if (preg_match("/\b$username\b/", $overrideList)) $permitted = true; // username is in permitted list
+
+            $q = $this->query("select 1 from redcap_user_allowlist where username=? limit 1", [$username]);
+            if (db_num_rows($q)>0) $permitted = true; // user is in allow list
+        } else {
+            // does specified user have the specified access permission?
+            $permitted = (bool)$rights[$userRight];
+        }
 
         return $permitted;
     }
@@ -108,7 +116,12 @@ class UserManagementTools extends AbstractExternalModule
     protected function getProjectAdmins() {
         $admins = array();
         foreach ($this->getProject()->getUsers() as $user) {
-            if ($this->pagePermitted($user->getUsername())) $admins[] = $user;
+            if (
+                    $this->hasPagePermission($user->getUsername(), 'user_rights') &&
+                    $this->hasPagePermission($user->getUsername())
+                ) {
+                $admins[] = $user;
+            }
         }
         return $admins;
     }
@@ -120,7 +133,7 @@ class UserManagementTools extends AbstractExternalModule
     public function redcap_user_rights($project_id) {
         $bad = array();
         foreach ($this->getProject()->getUsers() as $user) {
-            if (!$this->pagePermitted($user->getUsername())) $bad[] = $user->getUsername();
+            if (!$this->hasPagePermission($user->getUsername())) $bad[] = $user->getUsername();
         }
         if (count($bad) > 0) {
             echo '<div id="UserManagementTools_Info" class="yellow mb-1" style="max-width:850px;display:none;"><h6><i class="fas fa-exclamation-triangle mr-1"></i>'.static::TITLE_WARN.'</h6><p class="my-0">External users in project admin roles: '.implode(', ',$bad).'</p></div>';
